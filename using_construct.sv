6d8c8f744fe7438e9d5b38c3818941c311c31a41
Avronil <basuavronil65@gmail.com>
2026-03-26 17:06:34 +0530

    using_construct.sv

--- using_construct.sv
+++ using_construct.sv
@@ -1,7 +1,7 @@
 class transaction;
   bit [31:0] data;
   int id;
-  function new ( int m_data, int m_id);
+  function new ( int m_data, int m_id); //by default it is present inside the class we are just calling it 
     data = m_data;
     id = m_id;
   endfunction
