f764d27fbbcfca7f36923e6caf97e8122fc7980a
Avronil <basuavronil65@gmail.com>
2026-03-27 23:52:44 +0530

    Add examples of global and local variables inside constructors

--- global_local_variables.sv
+++ global_local_variables.sv
@@ -24,3 +24,47 @@ module tb;
     $display ( "%0d %0d", h1.a, h1.b); 
   end
 endmodule 
+
+
+// example 2
+// example
+class c1;
+  int a;
+  int b;
+  int c;
+  function new (int a, int b);
+    this.a = a;
+    this.b = b;
+    this.c = a + b;
+    $display ( "a = %0d b = %0d c = %0d ", a, b, c);
+  endfunction
+endclass 
+module m1;
+   c1 h1;
+  initial begin
+    h1 = new(2,3);
+    $display ("a_g = %0d b_g = %0d c_g = %0d", h1.a, h1.b, h1.c);
+  end
+endmodule 
+/* a = 2 b = 3 c = 5 
+a_g = 2 b_g = 3 c_g = 5*/
+
+class c2;
+  int a;
+  int b;
+  function new(int a, int b);
+    int c;
+    c = a + b;
+    $display ( "a = %0d b = %0d c = %0d", a, b, c );
+  endfunction
+endclass
+module m2;
+  c2 h2;
+  initial begin
+    h2 = new(2,3);
+    $display ("a_g = %0d b_g = %0d", h2.a, h2.b);
+  end
+endmodule
+
+*/a = 2 b = 3 c = 5
+a_g = 0 b_g = 0 */
