0000753f3b29c09cf6534787502fbc5baa5cd076
Avronil <basuavronil65@gmail.com>
2026-03-27 21:49:37 +0530

    static and automatic variables in single handle and multi handle
    Refactored class and module structure, updated variable declarations and initialization, and modified display outputs.

--- 2 handle assignments.sv
+++ 2 handle assignments.sv
@@ -1,16 +1,59 @@
 class c1;
-  int b;
-  bit [5:0] a;
+  int a;
+  static int b;
+  int c;
+  
+  function void one();
+    a++;
+    b++;
+    c++;
+    $display ("a = %0d",a);
+    $display ("b = %0d",b);
+    $display ("c = %0d",c);
+  endfunction
 endclass
+
 module tb;
-  c1 h1, h2, h3;
+  c1 h1;
   initial begin
-    h1 = new();
-    h2 = h1;
-    h3 = h1;
-    h2.a = 20;
-    h3.b = 10;
-    $display ("%0d %0d", h1.a, h1.b);
+      h1 = new();
+    repeat (3) begin
+      h1.one();
+    end
   end
 endmodule
-// a = 20 b = 10 
+/* 
+a = 1  b = 1  c = 1
+a = 2  b = 2  c = 2
+a = 3  b = 3  c = 3
+*/
+
+class c1;
+  int a;
+  static int b;
+  int c;
+  
+  function void one();
+    a++;
+    b++;
+    c++;
+    $display ("a = %0d",a);
+    $display ("b = %0d",b);
+    $display ("c = %0d",c);
+  endfunction
+endclass
+
+module tb;
+  c1 h[3];
+  initial begin
+    foreach (h[i]) begin
+      h[i] = new();
+      h[i].one();
+    end
+  end
+endmodule
+/*
+a = 1  b = 1  c = 1
+a = 1  b = 2  c = 1
+a = 1  b = 3  c = 1
+*/
